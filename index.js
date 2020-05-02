4b799a5d27799b4818829f632e87d1442d4a9281
Poonam Sagar <poonamsagar9212@gmail.com>
2020-05-02 15:36:49 +1000

    use of middleware

--- index.js
+++ index.js
@@ -1,91 +1,96 @@
-const express = require("express");
+const express = require('express');
+const requestLogger = require('./logger');
+const unknownEndpoint = require('./unknownRouteHandler');
 const app = express();
 
 app.use(express.json());
- 
-let persons=[
-    {
-        name:"Arto Hellas",
-        number: 67890000,
-        id : 1
-    },
-    {
-        name:"Ada Lovelace",
-        number: 34567567,
-        id: 2
-    },
-    {
-        name:"Dan Abramov",
-        number: 123432343,
-        id:3
-    },
-    {
-        name:"Mary Poppendieck",
-        number: 67898789,
-        id:4
-    },
-    {
-    name:"John",
-    number:56757787,
-    id: 5
-    },
+app.use(requestLogger);
+
+let persons = [
+  {
+    name: 'Arto Hellas',
+    number: 67890000,
+    id: 1,
+  },
+  {
+    name: 'Ada Lovelace',
+    number: 34567567,
+    id: 2,
+  },
+  {
+    name: 'Dan Abramov',
+    number: 123432343,
+    id: 3,
+  },
+  {
+    name: 'Mary Poppendieck',
+    number: 67898789,
+    id: 4,
+  },
+  {
+    name: 'John',
+    number: 56757787,
+    id: 5,
+  },
 ];
-app.get("/",(req,res)=>{
-    res.send("<h1>This is Phonebook rest api</h1>");
+app.get('/', (req, res) => {
+  res.send('<h1>This is Phonebook rest api</h1>');
 });
 
-app.get("/api/persons",(req,res)=>{
-    res.json(persons);
+app.get('/api/persons', (req, res) => {
+  res.json(persons);
 });
 
-app.get("/info",(req,res)=>{
-    res.send(`<h3>Phonebook has info for ${persons.length} people</h3><h3>${new Date()}</h3>`);
+app.get('/info', (req, res) => {
+  res.send(
+    `<h3>Phonebook has info for ${
+      persons.length
+    } people</h3><h3>${new Date()}</h3>`
+  );
 });
 
-app.get("/api/persons/:id",(request,response)=>{
-const id = Number(request.params.id);
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id);
 
-const person = persons.find((person) => person.id ===id);
-if(person){
-response.json(person);
-} else {
+  const person = persons.find((person) => person.id === id);
+  if (person) {
+    response.json(person);
+  } else {
     response.status(404).end();
-}
+  }
 });
 
-app.delete("/api/persons/:id", (request,response) =>{
-    const id = Number(request.params.id);
-    persons = persons.filter((person) => person.id !== id);
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter((person) => person.id !== id);
 
-    response.status(204).end();
+  response.status(204).end();
 });
 
-const generatedId = () =>
-  Math.floor( Math.random()*100000 +5);
+const generatedId = () => Math.floor(Math.random() * 100000 + 5);
 
- 
-app.post("/api/persons",(request, response) => {
-    const body = request.body;
-    if(!(body.name && body.number)){
-        return response.status(400).json({
-            error: "name or number is missing",
-        });
-    }
-    personExists=persons.some(person=>person.name===body.name);
-    if(!personExists){
-        const person ={
-            name: body.name,
-            number : body.number,
-            id: generatedId(),
-        };
-        persons = persons.concat(person);
-        response.json(person);
-    }else{
-        response.status(409).json({error:'name must be unique'})
-    }
-    
-
+app.post('/api/persons', (request, response) => {
+  const body = request.body;
+  if (!(body.name && body.number)) {
+    return response.status(400).json({
+      error: 'name or number is missing',
+    });
+  }
+  personExists = persons.some((person) => person.name === body.name);
+  if (!personExists) {
+    const person = {
+      name: body.name,
+      number: body.number,
+      id: generatedId(),
+    };
+    persons = persons.concat(person);
+    response.json(person);
+  } else {
+    response.status(409).json({ error: 'name must be unique' });
+  }
 });
+
+app.use(unknownEndpoint);
 
 const PORT = 3001;
 app.listen(PORT);
