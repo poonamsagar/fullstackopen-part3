c21f42a947ef4328af622b822431d0807e0a4955
Poonam Sagar <poonamsagar9212@gmail.com>
2020-05-03 17:29:51 +1000

    using backend with frontend

--- part-self-learning/backend/index.js
+++ part-self-learning/backend/index.js
@@ -1,37 +1,40 @@
-const express = require("express");
+const express = require('express');
 const app = express();
+const cors = require('cors');
 
+app.use(cors());
 app.use(express.json());
+app.use(express.static('build'));
 
 let notes = [
   {
     id: 1,
-    content: "HTML is easy",
-    date: "2019-05-30T17:30:31.098Z",
+    content: 'HTML is easy',
+    date: '2019-05-30T17:30:31.098Z',
     important: true,
   },
   {
     id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2019-05-30T18:39:34.091Z",
+    content: 'Browser can execute only Javascript',
+    date: '2019-05-30T18:39:34.091Z',
     important: false,
   },
   {
     id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2019-05-30T19:20:14.298Z",
+    content: 'GET and POST are the most important methods of HTTP protocol',
+    date: '2019-05-30T19:20:14.298Z',
     important: true,
   },
 ];
-app.get("/", (req, res) => {
-  res.send("<h1>Hello World</h1>");
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World</h1>');
 });
 
-app.get("/api/notes", (req, res) => {
+app.get('/api/notes', (req, res) => {
   res.json(notes);
 });
 
-app.get("/api/notes/:id", (request, response) => {
+app.get('/api/notes/:id', (request, response) => {
   const id = Number(request.params.id);
 
   const note = notes.find((note) => note.id === id);
@@ -42,7 +45,7 @@ app.get("/api/notes/:id", (request, response) => {
   }
 });
 
-app.delete("/api/notes/:id", (request, response) => {
+app.delete('/api/notes/:id', (request, response) => {
   const id = Number(request.params.id);
   notes = notes.filter((note) => note.id !== id);
 
@@ -54,12 +57,12 @@ const generatedId = () => {
   return maxId + 1;
 };
 
-app.post("/api/notes", (request, response) => {
+app.post('/api/notes', (request, response) => {
   const body = request.body;
 
   if (!body.content) {
     return response.status(400).json({
-      error: "content is missing",
+      error: 'content is missing',
     });
   }
   const note = {
@@ -75,6 +78,6 @@ app.post("/api/notes", (request, response) => {
   response.json(note);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT);
 console.log(`Server running on port ${PORT}`);
