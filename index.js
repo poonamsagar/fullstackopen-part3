8d9b6345c5731e8ee871185443f17bf7c0ad35b8
Poonam Sagar <poonamsagar9212@gmail.com>
2020-05-03 17:55:12 +1000

    3.9 phonebook backend step9 completed

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const morgan = require('morgan');
+const cors = require('cors');
 const express = require('express');
 const app = express();
+
 morgan.token('body', (req) => JSON.stringify(req.body));
+app.use(cors());
 app.use(express.json());
 app.use(
   morgan(':method :url :status :res[content-length] - :response-time ms :body')
