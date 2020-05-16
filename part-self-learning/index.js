7e9394dc87b0830ef219bb23d72aedc7bdbcc634
Poonam Sagar <poonamsagar9212@gmail.com>
2020-05-16 14:16:05 +1000

    Created models by using mongoose

--- part-self-learning/index.js
+++ part-self-learning/index.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const Note = require('./models/note');
 
 app.use(cors());
 app.use(express.json());
@@ -31,18 +33,20 @@ app.get('/', (req, res) => {
 });
 
 app.get('/api/notes', (req, res) => {
-  res.json(notes);
+  Note.find({}).then((notes) => {
+    res.json(notes.map((note) => note.toJSON()));
+  });
 });
 
 app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id);
-
-  const note = notes.find((note) => note.id === id);
-  if (note) {
-    response.json(note);
-  } else {
-    response.status(404).end();
-  }
+  Note.findById(request.params.id)
+    .then((note) => {
+      response.json(note.toJSON());
+    })
+    .catch((error) => {
+      console.log('An error occured', error.message);
+      response.status(404).end();
+    });
 });
 
 app.delete('/api/notes/:id', (request, response) => {
@@ -65,19 +69,17 @@ app.post('/api/notes', (request, response) => {
       error: 'content is missing',
     });
   }
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
     date: new Date(),
-    id: generatedId(),
-  };
+  });
 
-  notes = notes.concat(note);
-  console.log(note);
-
-  response.json(note);
+  note.save().then((savedNote) => {
+    response.json(savedNote.toJSON());
+  });
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT);
 console.log(`Server running on port ${PORT}`);
